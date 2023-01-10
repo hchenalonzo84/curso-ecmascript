6f8d131c549db2594ee74924382f7161c510acab
unknown <roland8410@hotmail.com>
2023-01-10 00:05:08 -0600

    version 1.0.0

---
+++ src/es6/03-string.js
@@ -0,0 +1,17 @@
+let hello = 'hello';
+let world = 'World';
+let epicPhrase = hello+ ' ' +world+'!!';
+console.log(epicPhrase);
+
+// template literals
+ let epicPhrase2=`${hello} ${world}!!`
+
+ //multi-line strings
+  let lorem = 'esto es un string \n'+'esto es otra linea';
+
+  let lorem2=`esto es una grase epica
+   la continuacion de la frase epica 2 
+  `;
+
+  console.log(lorem);
+  console.log(lorem2);
