d9feef289e9b624cabbd94a69c006d7c1f145dae
unknown <roland8410@hotmail.com>
2023-01-09 23:44:30 -0600

    version 1.0.0

---
+++ src/es6/00-let-const.js
@@ -0,0 +1,25 @@
+var lastName= 'david';
+lastName= 'oscar';
+console.log(lastName);
+
+let fruit = 'manzana';
+fruit = 'kiwi';
+console.log(fruit);
+
+const animal='dog';
+animal= 'cat';
+console.log(animal);
+
+const fruits = () =>{
+
+if (true) {
+    var fruit1= 'apple';// function scope
+    let fruit2= 'kiwi'; //block scope
+    const fruit3= 'banana';// block scope
+}
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruit3);
+}
+
+fruits();
